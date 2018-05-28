b8e6f471e1a880f2b2b06308306a3829f4a02c80
HieuNV <hieunv@live.com>
2018-05-28 13:10:09 +0700

    update prettier settings

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,24 +1,14 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import {
-    NotificationComponent,
-    NotificationContainerComponent
-} from 'ngx-notification';
+import { NotificationComponent, NotificationContainerComponent } from 'ngx-notification';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('AppComponent', () => {
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            imports: [
-                HttpClientModule,
-                HttpClientTestingModule
-            ],
-            declarations: [
-                AppComponent,
-                NotificationComponent,
-                NotificationContainerComponent
-            ]
+            imports: [HttpClientModule, HttpClientTestingModule],
+            declarations: [AppComponent, NotificationComponent, NotificationContainerComponent]
         }).compileComponents();
     }));
     it('should create the app', async(() => {
@@ -29,14 +19,12 @@ describe('AppComponent', () => {
     // it(`should have as title 'app'`, async(() => {
     //     const fixture = TestBed.createComponent(AppComponent);
     //     const app = fixture.debugElement.componentInstance;
-    //     expect(app.title).toEqual('app');
+    //     expect(app.title).toEqual('@tickify/ngx-notification');
     // }));
-    // it('should render title in a h1 tag', async(() => {
-    //     const fixture = TestBed.createComponent(AppComponent);
-    //     fixture.detectChanges();
-    //     const compiled = fixture.debugElement.nativeElement;
-    //     expect(compiled.querySelector('h1').textContent).toContain(
-    //         'Welcome to app!'
-    //     );
-    // }));
+    it('should render title in a h1 tag', async(() => {
+        const fixture = TestBed.createComponent(AppComponent);
+        fixture.detectChanges();
+        const compiled = fixture.debugElement.nativeElement;
+        expect(compiled.querySelector('h1').textContent).toContain('@tickify/ngx-notification');
+    }));
 });
